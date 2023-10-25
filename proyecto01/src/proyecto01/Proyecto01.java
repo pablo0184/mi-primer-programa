4d4ad7d80cd9504228534e2ef61736f28c1798a8
pablo <pablo.velezleon1@icloud.com>
2023-10-24 22:23:35 -0500

    segundo commit

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -14,6 +14,7 @@ public class Proyecto01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("hola mundo");
         // TODO code application logic here
     }
     
